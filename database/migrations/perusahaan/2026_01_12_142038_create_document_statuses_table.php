1aa70fe100588b7c8a766c215ec6a86b7723f88d
DomicMae1 <ardon.yunors@tako.co.id>
2026-01-13 16:24:33 +0700

    fix: resolve migration issues

--- database/migrations/perusahaan/2026_01_12_142038_create_document_statuses_table.php
+++ database/migrations/perusahaan/2026_01_12_142038_create_document_statuses_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('status'); 
             
             // User ID yang melakukan aksi
-            $table->unsignedBigInteger('by'); 
+            $table->string('by')->nullable();
             
             $table->timestamps();
 
